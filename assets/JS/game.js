443372435695ebcda64c417406fdd5c811c67f42
TheNoodleMoose <cjh605@gmail.com>
2018-11-14 09:31:44 -0500

    adding initial files

---
+++ assets/JS/game.js
@@ -0,0 +1,140 @@
+console.log("This works!")
+
+var game = {
+    question1: {
+        question: "What is the name of the character with eight tentacles in Spongebob?",
+        answer: "Squidward",
+        wrong1: "Plankton",
+        wrong2: "Patrick",
+        wrong3: "Old Man Jennkins"
+    },
+    question2: {
+        question: "Where does Spongebob work at?",
+        answer: "Krusty Krab",
+        wrong1: "Krusty Dogs",
+        wrong2: "Chum Bucket",
+        wrong3: "Rusty Shack"
+    },
+
+    question3: {
+        question: "Who is Spongebobs best friend?",
+        answer: "Patrick",
+        wrong1: "Squidward",
+        wrong2: "Sandy",
+        wrong3: "Larry"
+    },
+
+    question4: {
+        question: "What is the name of Spongebob's Cousin",
+        answer: "Stanley",
+        wrong1: "Steve",
+        wrong2: "Sean",
+        wrong3: "Stephen"
+    },
+
+    question5: {
+        question: "What is the name of Patrick's pet rock?",
+        answer: "Rocky",
+        wrong1: "Pebble",
+        wrong2: "Stoney",
+        wrong3: "Patrock"
+    },
+
+    gamestage: 0,
+    rightAnswers: 0,
+    wrongAnswers: 0,
+    chosen: "",
+
+    startGame: function () {
+        game.gamestage = 0;
+        $("#question").html(game.question1.question)
+        $("#aAnswer").html(game.question1.answer)
+        $("#bAnswer").html(game.question1.wrong1)
+        $("#cAnswer").html(game.question1.wrong2)
+        $("#dAnswer").html(game.question1.wrong3)
+    },
+}
+
+game.startGame();
+
+$(".answer").on("click", function () {
+    game.chosen = $(this).text()
+    if (game.gamestage === 0) {
+        if (game.chosen === game.question1.answer) {
+            game.rightAnswers++;
+            game.gamestage++;
+            game.chosen = "";
+            $("#question").html(game.question2.question)
+            $("#aAnswer").html(game.question2.wrong3)
+            $("#bAnswer").html(game.question2.wrong1)
+            $("#cAnswer").html(game.question2.wrong2)
+            $("#dAnswer").html(game.question2.answer)
+            console.log("Right: " +game.rightAnswers)
+
+        }
+        else {
+            game.wrongAnswers++;
+            game.gamestage++;
+            game.chosen = "";
+            $("#question").html(game.question2.question)
+            $("#aAnswer").html(game.question2.wrong3)
+            $("#bAnswer").html(game.question2.wrong1)
+            $("#cAnswer").html(game.question2.wrong2)
+            $("#dAnswer").html(game.question2.answer)
+            console.log("Wrong:" +game.wrongAnswers)
+        }
+
+    }
+    
+    if (game.gamestage === 1 && game.chosen != "") {
+        if (game.chosen === game.question2.answer) {
+            game.rightAnswers++;
+            game.gamestage++;
+            game.chosen = "";
+            $("#question").html(game.question3.question)
+            $("#aAnswer").html(game.question3.wrong3)
+            $("#bAnswer").html(game.question3.answer)
+            $("#cAnswer").html(game.question3.wrong1)
+            $("#dAnswer").html(game.question3.wrong2)
+            console.log("Right:" +game.rightAnswers)
+        }
+        else {
+            game.wrongAnswers++;
+            game.gamestage++;
+            game.chosen = "";
+            $("#question").html(game.question3.question)
+            $("#aAnswer").html(game.question3.wrong3)
+            $("#bAnswer").html(game.question3.answer)
+            $("#cAnswer").html(game.question3.wrong1)
+            $("#dAnswer").html(game.question3.wrong2)
+            console.log("Wrong:" +game.wrongAnswers)
+
+        }
+    }
+    if (game.gamestage === 2 && game.chosen != "") {
+        if (game.chosen === game.question3.answer) {
+            game.rightAnswers++;
+            game.gamestage++;
+            game.chosen = "";
+            $("#question").html(game.question4.question)
+            $("#aAnswer").html(game.question4.answer)
+            $("#bAnswer").html(game.question4.wrong1)
+            $("#cAnswer").html(game.question4.wrong2)
+            $("#dAnswer").html(game.question4.wrong3)
+            console.log("Right:" +game.rightAnswers)
+        }
+        else {
+            game.wrongAnswers++;
+            game.gamestage++;
+            game.chosen = "";
+            $("#question").html(game.question4.question)
+            $("#aAnswer").html(game.question3.answer)
+            $("#bAnswer").html(game.question3.wrong1)
+            $("#cAnswer").html(game.question3.wrong2)
+            $("#dAnswer").html(game.question3.wrong3)
+            console.log("Wrong:" +game.wrongAnswers)
+
+        }
+    }
+
+})
