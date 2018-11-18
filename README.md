6bc5b2863e6e44e4c55f0ec088fd6635d6c197a8
TheNoodleMoose <cjh605@gmail.com>
2018-11-18 16:32:13 -0500

    Added comments to all files and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# TriviaGame
+# Spongebob Trivia Game
+## Welcome to my readme for my Spongebob Trivia Game! Created with Jquery, JavaScript, HTML, and BootStrap
+This is a simple trivia game with only ten questions so far. Eventually, I may add more as it is easily scaleable.
+# How To Play
+To start playing, simply click the start button and the first question will be displayed. 
+Each question has a timer of 15 seconds to answer the question. 
+
+![start](assets/images/readme/Start.jpg)
+
+If you do not answer in time it will counted as a wrong answer.
+
+![timer](assets/images/readme/timer.jpg)
+
+# End of Game
+So far there are only ten questions, so once you answer them all it will display how many you got right and you can try the quiz again if you wish. Thanks for taking the time to check my quiz out! 
+
+![try-again](assets/images/readme/try-again.jpg)
--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,8 +1,15 @@
+// Var for the timer
 var intervalId;
 
+//Game object that holds all the questions and functions for the game
 var game = {
-
+    //Questions array 
     questions: [
+        //Each question objectnis structured the same
+        // The first line is for the questions being asked
+        // The second line is a answers array which all four possible answers are stored
+        // The third line is the correct answers as a string, used to compare to what the users clicks on
+        // The final line is the stage the question is on, used to check which question will be displayed
         question1 = {
             question: "What is the name of the character with eight tentacles in Spongebob?",
             answers: ["Plankton", "Squidward", "Patrick", "Old Man Jennkins"],
@@ -75,14 +82,20 @@ var game = {
     ],
 
 
-
+    // Used to check which question will be displayed
     stage: 1,
+    // Used to store the amount of correct answers
     rightAnswers: 0,
+    // Used to store the amount of wrong answers
     wrongAnswers: 0,
+    // This holds the text of the answer the user clicks on and checks it against the correct answer of that question
     chosen: "",
+    // Used for the timer
     time: 15,
+    // Used to determine if the clock is running
     clockRunning: false,
 
+    // Start game function that resets everything back and displays the gary gif
     startGame: function () {
         game.stage = 1;
         game.wrongAnswers = 0;
@@ -93,7 +106,9 @@ var game = {
         $(".answersWrong").text("")
         $(".giphy-sticker").attr("src", "https://giphy.com/embed/YCAZI58mofD6E");
     },
-
+    // This is used to display the questions
+    // For game.questions array if the game.stage is equal to the stage of game.questions[i], than display the questions.
+    // Also display all the possible answers
     questionChooser: function () {
         for (i = 0; i < game.questions.length; i++) {
             if (game.stage === game.questions[i].stage) {
@@ -104,10 +119,12 @@ var game = {
                 $("#cAnswer").text(game.questions[i].answers[2])
                 $("#dAnswer").text(game.questions[i].answers[3])
             }
-
         }
     },
-
+    // This is used to check if the answer is correct
+    // If what the user clicks on is equal to the string of the correct answer of that particular question,
+    // Than add to teh game stage, add to rightanswers, and then reset what they choose back to an empty string
+    // Then run question choose again to display the next set of questions and answers
     checkAnswer: function () {
         for (i = 0; i < game.questions.length; i++) {
             if (game.chosen === game.questions[i].correctAnswer && game.stage === game.questions[i].stage) {
@@ -119,7 +136,12 @@ var game = {
             }
         }
     },
-
+    // Used to check if what the user clicks on is the wrong answer
+    // If what they click on is not equal to the string of the correct answer AND its not equal to a empty string than 
+    // Up the game stage, add to wrong answers, reset the chosen value to an empty string and run question chooser
+    // The reason for the game.chosen != "" is because check answer and check wrong run at the same time so when it
+    // Goes to the next stage its reset to an empty string and it automatically scored that as wrong. This fixed it.
+    // Also if the time runs out than that counts as a wrong answer
     checkWrong: function () {
         for (i = 0; i < game.questions.length; i++) {
             if (game.chosen != game.questions[i].answer && game.chosen != "" || game.time === 0) {
@@ -131,19 +153,21 @@ var game = {
             }
         }
     },
-
+    // This is used to check if the game is over by comparing the gam.e stage to length of game.questions plus 1.
     checkGameOver: function () {
         if (game.stage === game.questions.length + 1) {
             this.stopGame();
         }
     },
-
+    // Used for the timer
     timer: function () {
         if (!game.clockRunning) {
             intervalId = setInterval(game.count, 1000)
             game.clockRunning = true;
         }
     },
+    // This displays the clock counting down and if it runs out than set game.time back to fifteen 
+    // and add to their wrong answers
     count: function () {
         if (game.time > 0 && game.clockRunning === true) {
             game.time--;
@@ -158,12 +182,13 @@ var game = {
 
         }
     },
-
+    // Resets the timer
     timerReset: function () {
         game.time = 15;
         $("#time").text(`${game.time} Seconds Left`);
     },
-
+    // This stops the game and displays how many the user got right/wrong. Clears the timer and stops the clock and
+    // displays the spongebob dancing gif
     stopGame: function () {
         $(".answer").text("");
         $("#question").text("");
@@ -177,20 +202,21 @@ var game = {
     }
 }
 
+// When the user clicks the start button than start the game and start the timer
 $(".start").on("click", function () {
     game.startGame();
     game.timer();
     game.count();
 })
-
+// When the user clicks the try again button than reset the game and start again
 $(".try-again").on("click", function () {
     game.startGame();
     game.timer();
     game.count();
 })
-
-
-
+// This is used to check the value of what the used clicks on. The text
+// of what they choose of is stored as a variable called game.chosen and
+// game.checkAnswer and checkWrong run to check if game.chosen is correct or incorrect
 $(".answer").on("click", function () {
     game.chosen = $(this).text()
 
